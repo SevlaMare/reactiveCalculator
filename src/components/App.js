1e6cb9fa9e2f9668d7c4186261b40e944b00d896
Mare <sevilamare@gmail.com>
2020-09-17 18:21:17 -0300

    app component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import Display from './Display';
+// import ButtonPanel from './ButtonPanel';
+
+function App() {
+  return (
+    <React.Fragment>
+      Hi From app
+    </React.Fragment>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './css/style.css';
-import PropTypes from 'prop-types';
+import App from './components/App';
 
-const Header = props => {
-  const { title } = props;
-
-  return (
-    <h1>
-      Hi from ES6
-      {title}
-    </h1>
-  );
-};
-
-Header.defaultProps = {
-  title: '',
-};
-
-Header.propTypes = {
-  title: PropTypes.string,
-};
-
-const Main = () => {
-  const [title] = useState('av');
-
-  return (
-    <Header title={title} />
-  );
-};
-
-ReactDOM.render(<Main />, document.querySelector('#app'));
+ReactDOM.render(<App />, document.querySelector('#app'));
