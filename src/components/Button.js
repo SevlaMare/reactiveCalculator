45d65935510a83dc11b1dddc226dc8b48b51beab
Mare <sevilamare@gmail.com>
2020-09-18 13:54:58 -0300

    button prop for width

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = props => {
-  const { buttonName, color } = props;
+  const { buttonName, wide, color } = props;
+
   return (
-    <button type="button"
-      style={{ backgroundColor: color}}
-    >
+    <button type="button" style={{
+      backgroundColor: color,
+      width: (wide ? '50%' : '25%')
+    }}>
       { buttonName }
     </button>
   );
@@ -14,11 +16,13 @@ const Button = props => {
 
 Button.propTypes = {
   buttonName: PropTypes.string,
+  wide: PropTypes.bool,
   color: PropTypes.string,
 };
 
 Button.defaultProps = {
   buttonName: '',
+  wide: false,
   color: 'orange',
 };
 
