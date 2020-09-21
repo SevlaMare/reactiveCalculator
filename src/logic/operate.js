677c0c01610ec4972a3c21ea5cc117261f27167a
Mare <sevilamare@gmail.com>
2020-09-21 18:17:16 -0300

    wrap div in conditional

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,18 +3,25 @@ import Big from 'big.js';
 const Operate = (numberOne, numberTwo, operation) => {
   if(numberOne) { const x1 = Big(numberOne); }
   if(numberTwo) { const x2 = Big(numberTwo); }
+  let result;
 
-  if (operation === '+') x1.plus(x2);
-  if (operation === '-') x1.minus(x2);
-  if (operation === 'X') x1.times(x2);
+  if (operation === '+') result = x1.plus(x2);
+  if (operation === '-') result = x1.minus(x2);
+  if (operation === 'X') result = x1.times(x2);
 
-  if (x2.toString() === '0') "Can't divide by zero";
-  if (operation === '÷') x1.div(x2);
+  if (operation === '÷') {
+    if (x2.toString() === '0') result = "Can't divide by zero";
+    return result = x1.div(x2);
+  }
 
-  if (operation === '%') x1.div(x2)/100;
-  if (operation === '+/-') x1.times(-1);
+  if (operation === '%') {
+    if (x2.toString() === '0') result = "Can't divide by zero";
+    return x1.div(x2)/100;
+  }
 
-  return x1
+  if (operation === '+/-') result = x1.times(-1);
+
+  return result.toString()
 };
 
 export default Operate;
