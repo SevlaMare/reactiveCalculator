65e8b11e733479f76a3b2896566b8985c2c3ae9e
Mare <sevilamare@gmail.com>
2020-09-22 20:04:34 -0300

    standard function name

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,42 +3,42 @@ import PropTypes from 'prop-types';
 import Button from './Button';
 
 const ButtonPanel = props => {
-  const handleClickBP = buttonName => props.clickHandler(buttonName);
+  const handleClick = buttonName => props.clickHandler(buttonName);
 
   return (
     <div className="flex flex-col btnPanel">
       <div className="rowPanel">
-        <Button color="#d3d3d3" buttonName="AC" clickHandler={handleClickBP} />
-        <Button color="#d3d3d3" buttonName="+/-" clickHandler={handleClickBP} />
-        <Button color="#d3d3d3" buttonName="%" clickHandler={handleClickBP} />
-        <Button buttonName="÷" clickHandler={handleClickBP} />
+        <Button color="#d3d3d3" buttonName="AC" clickHandler={handleClick} />
+        <Button color="#d3d3d3" buttonName="+/-" clickHandler={handleClick} />
+        <Button color="#d3d3d3" buttonName="%" clickHandler={handleClick} />
+        <Button buttonName="÷" clickHandler={handleClick} />
       </div>
 
       <div className="rowPanel">
-        <Button color="#d3d3d3" buttonName="7" clickHandler={handleClickBP} />
-        <Button color="#d3d3d3" buttonName="8" clickHandler={handleClickBP} />
-        <Button color="#d3d3d3" buttonName="9" clickHandler={handleClickBP} />
-        <Button buttonName="X" clickHandler={handleClickBP} />
+        <Button color="#d3d3d3" buttonName="7" clickHandler={handleClick} />
+        <Button color="#d3d3d3" buttonName="8" clickHandler={handleClick} />
+        <Button color="#d3d3d3" buttonName="9" clickHandler={handleClick} />
+        <Button buttonName="X" clickHandler={handleClick} />
       </div>
 
       <div className="rowPanel">
-        <Button color="#d3d3d3" buttonName="4" clickHandler={handleClickBP} />
-        <Button color="#d3d3d3" buttonName="5" clickHandler={handleClickBP} />
-        <Button color="#d3d3d3" buttonName="6" clickHandler={handleClickBP} />
-        <Button buttonName="-" clickHandler={handleClickBP} />
+        <Button color="#d3d3d3" buttonName="4" clickHandler={handleClick} />
+        <Button color="#d3d3d3" buttonName="5" clickHandler={handleClick} />
+        <Button color="#d3d3d3" buttonName="6" clickHandler={handleClick} />
+        <Button buttonName="-" clickHandler={handleClick} />
       </div>
 
       <div className="rowPanel">
-        <Button color="#d3d3d3" buttonName="1" clickHandler={handleClickBP} />
-        <Button color="#d3d3d3" buttonName="2" clickHandler={handleClickBP} />
-        <Button color="#d3d3d3" buttonName="3" clickHandler={handleClickBP} />
-        <Button buttonName="+" clickHandler={handleClickBP} />
+        <Button color="#d3d3d3" buttonName="1" clickHandler={handleClick} />
+        <Button color="#d3d3d3" buttonName="2" clickHandler={handleClick} />
+        <Button color="#d3d3d3" buttonName="3" clickHandler={handleClick} />
+        <Button buttonName="+" clickHandler={handleClick} />
       </div>
 
       <div className="rowPanel">
-        <Button color="#d3d3d3" wide="true" buttonName="0" clickHandler={handleClickBP} />
-        <Button color="#d3d3d3" buttonName="." clickHandler={handleClickBP} />
-        <Button buttonName="=" clickHandler={handleClickBP} />
+        <Button color="#d3d3d3" wide="true" buttonName="0" clickHandler={handleClick} />
+        <Button color="#d3d3d3" buttonName="." clickHandler={handleClick} />
+        <Button buttonName="=" clickHandler={handleClick} />
       </div>
     </div>
   );
